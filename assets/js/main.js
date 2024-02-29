58d5d2b077a6402df5197dd0176e217a19703682
Roia Tembo <roia@itmogul.itmogul.co.za>
2024-02-29 12:10:57 +0200

    mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,31 @@
 var splide = new Splide(".splide");
 splide.mount();
+
+var viewportWidth = window.innerWidth;
+
+if (viewportWidth < 440) {
+  $(".top-bar-item").html(
+    "BOOK TICKETS NOW <a href='' style='text-decoration: underline'>LEARN MORE</a>"
+  );
+
+  $(".title-text").text("SQUAMISH VALLEY ON THE EDGE");
+
+  $(".explore-section").prepend(
+    "<img width='100%' height='255px' src='/assets/img/background.png'>"
+  );
+
+  $(".explore-title").text("DESTINATIONS");
+
+  $(".tours-and-tickets").prepend(
+    "<img class='mobile-image' src='/assets/img/inside_tent.png' />"
+  );
+
+  var lastFindYourNextCard = $(".find-your-next-card")[1];
+  lastFindYourNextCard.style.display = "none";
+
+  $(".find-your-next-title").text("FIND YOUR NEXT ADVENTURE");
+
+  $(".find-your-next-paragraph").text(
+    "We're here to help you dream up your next trip, figure out the details, and reserve experiences at over 4,200 facilities and 113,000 individual sites across the country. There's something for everyone on Recreation.gov, so get out there, experience the USA, and bring home a story!"
+  );
+}
